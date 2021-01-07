8731bc71bfc9886daec3ec3815b98045209e3aaa
teresa229 <scarlett_29@naver.com>
2021-01-07 18:14:42 +0900

    1st

---
+++ WebContent/WEB-INF/list.jsp
@@ -0,0 +1,50 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page import = "java.util.List" %>
+<%@ page import = "com.javaex.vo.PersonVo" %>
+    
+<%-- 데이터를 꺼내온다.--%>
+<%--  controller 포워드 시켰다. 떠내기만 하면 된다. dao에서 가져올 필요 없다. personList --%>
+
+<%
+	List<PersonVo> personList = (List<PersonVo>)request.getAttribute("pList"); //get,set 오브젝트로 받아올수 없으므로 형변환을 해준다.
+	System.out.println("====list.jsp====");
+	System.out.println(personList); //println은 toString을 불러오게 약속이 되어있다.
+%>
+
+<%-- dao에서 불러오는 것은 없다는 것 확인. 에티튜드가 해주었다--%>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>전화번호 리스트</h1>
+	
+	<p>
+	입력한 정보 내역입니다.
+	</p>
+	
+	<%for(int i=0; i<personList.size(); i++) {//섞는다고 표현..html에~ %> 
+	<table border = "1">
+		<tr>
+			<td>이름(name)</td>
+			<td><%=personList.get(i).getName() %></td>
+		</tr>
+		<tr>
+			<td>핸드폰(hp)</td>
+			<td><%=personList.get(i).getHp() %></td>
+		</tr>
+		<tr>
+			<td>회사(company)</td>
+			<td><%=personList.get(i).getCompany() %></td>
+		</tr>
+	
+	</table>
+	<%} %>
+	<a href="/phonebook2/pbc?action=wform">추가번호 등록</a>
+
+</body>
+</html>
---
+++ src/com/javaex/controller/PhoneController.java
@@ -0,0 +1,78 @@
+package com.javaex.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javaex.dao.PhoneDao;
+import com.javaex.vo.PersonVo;
+
+@WebServlet("/pbc")  //임의의 주소를 주었다. 파일이름과 대문자/소문자 동일해야 한다. 이름을 바꿀수도 있다.
+public class PhoneController extends HttpServlet { //상속받았다:extends HttpServlet
+    
+	/* Get 방식 구현 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//컨트롤러 테스트
+		System.out.println("controller");
+		
+		//파라미터 action값을 읽어서 request에 들어있다.
+		String action = request.getParameter("action");
+		System.out.println(action);
+		
+		//action은 문자열
+		if("list".equals(action)) { //action.equals("list") -> "list".equals(action) : null이 생기지 않게 위치 바꾼다.
+			System.out.println("리스트 처리");
+					
+			//리스트 풀력 처리
+			PhoneDao phoneDao = new PhoneDao();
+			List<PersonVo> personList = phoneDao.getPersonList(); //리스트를 준다.
+			
+			//System.out.println(personList.toString()); //테스트용
+			
+			//html -->엄청 복잡하다. --> jsp에서 짜는 것이 편하다.
+			
+			//데이터 전달
+			request.setAttribute("pList", personList); // (정해준 이름/주소)
+			
+			//jsp에 포워드 시킨다.
+			RequestDispatcher rd = request.getRequestDispatcher("./WEB-INF/list.jsp"); //jsp파일 위치 : 문자열로 알려준다.
+			rd.forward(request, response);
+		
+		} else if("wform".equals(action)) {
+			System.out.println("등록 폼 처리");
+					
+			//포워드 가져가다 사용한다.
+			RequestDispatcher rd = request.getRequestDispatcher("./WEB-INF/writeForm.jsp");
+			rd.forward(request, response);
+			
+		} else if("insert".equals(action)) {
+			System.out.println("전화번호 저장");
+			
+			//파라미터 3개 값
+			String name = request.getParameter("name");
+			String hp =request.getParameter("hp");
+			String company =request.getParameter("company");
+			
+			//personVo묶고
+			PersonVo personVo = new PersonVo(name,hp,company);
+			
+			//new dao --> 저장
+			PhoneDao phoneDao = new PhoneDao();
+			phoneDao.personInsert(personVo);
+			
+			response.sendRedirect("/phonebook2/pbc?action=list");
+		}
+	}
+
+	/* Post 방식 구현 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
